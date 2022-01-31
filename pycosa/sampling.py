ccfec226904c438f35adbe593e80a2546a36653b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-01-31 15:41:29 +0100

    buggedy bug

--- pycosa/sampling.py
+++ pycosa/sampling.py
@@ -436,7 +436,7 @@ class SampleStore:
     def __init__(self, df: pd.DataFrame, **kwargs):
         self.df = df
 
-    def symmetric_sample(sample, options, size=30):
+    def symmetric_sample(self, sample, options, size=30):
         """
         This sampling strategy extracts pairs of configuratins that differ exactly in the
         set of options specified. Therefore, one can estimate the effect of one or more options
